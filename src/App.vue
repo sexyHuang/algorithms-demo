6031936ea217272bd652acac149a77bfa6aa4045
huangjianyong <huangjianyong@tuzhanai.com>
2021-01-05 15:11:40 +0800

    feat: delete img

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <img :src="logoImg" />
   <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import logoImg from './assets/logo.png';
 
 export default defineComponent({
   name: 'App',
   components: {},
   setup() {
     return {
-      logoImg
     };
   }
 });
