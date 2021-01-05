c7734a880a7b7c614bce24f79e19197505cf8d3c
huangjianyong <huangjianyong@tuzhanai.com>
2021-01-05 17:26:51 +0800

    chore: something change

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const cdnBaseUrl = './';
 
 module.exports = {
+  base: cdnBaseUrl,
   outDir: 'algorithms-demo',
   alias: {
     '/src/': path.resolve(__dirname, 'src/')
