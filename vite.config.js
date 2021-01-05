29298e7154b8efac2884c6e7b83a9ae53d137b97
huangjianyong <huangjianyong@tuzhanai.com>
2021-01-05 15:28:10 +0800

    ci: something change

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-const path = require("path");
-const cdnBaseUrl = "./";
+const path = require('path');
+const cdnBaseUrl = './';
 
 module.exports = {
+  outDir: 'algorithms-demo',
   alias: {
-    "/src/": path.resolve(__dirname, "src/"),
-  },
+    '/src/': path.resolve(__dirname, 'src/')
+  }
 };
